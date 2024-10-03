b79d66030bd2a5516a5114ac026ddcb9fe001ebb
abhi-theCoder <119046323+abhi-theCoder@users.noreply.github.com>
2024-10-03 21:01:28 +0530

    Add files via upload

---
+++ Hello Nodejs/server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine','ejs');
+app.listen(port,()=>{
+    console.log(`server is running at port ${port}`);
+})
+app.get('/',(req,res)=>{
+    res.render("Hello");
+})
